32463f274e1fe73b241b26bbbe853953643da2f3
Akshit Vaishnav <akshitvaishnav96@gmail.com>
2021-07-07 10:16:31 +0530

    Update Insample_prediction_rnn.py

--- Insample_prediction_rnn.py
+++ Insample_prediction_rnn.py
@@ -2,9 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pandas import read_csv
 import math
-from keras.layers import SimpleRNN
-from keras.models import Sequential
-from keras.layers import Dense
+from tensorflow.keras.layers import SimpleRNN
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
 
